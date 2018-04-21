0b5952976ded16e29082bb3533fcf05c39bc986c
Daniel Doughty <djdoughty@gmail.com>
2018-04-21 05:45:42 +0000

    automatically backed up by learn

---
+++ lib/current_player.rb
@@ -0,0 +1,22 @@
+def turn_count (board)
+  count = 0 
+  board.each do |position|
+    if position == "X" || position == "O"
+      count += 1 
+    end
+  end
+  return count
+end
+
+def current_player (board)
+  if turn_count(board) == 0 || turn_count(board) % 2 != 0  
+    puts "X"
+  else 
+    puts "O"
+  end
+end
+
+current_player([" ", " ", " ", "O"])  
+  
+  
+  
